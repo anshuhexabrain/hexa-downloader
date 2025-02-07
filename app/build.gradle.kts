8ea9eb837b2dff92603a226fef2732203ffa9fd8
AnshumanHexabrain <anshuman@hexabrain.com>
2025-02-07 15:10:39 +0530

    changed JVM target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
@@ -46,5 +46,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+//    implementation ("com.github.anshuhexabrain:hexa-downloader:v1.0")
 }
